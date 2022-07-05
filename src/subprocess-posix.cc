ae061869a13d9f0130ec251a6c5cfe4406dd6672
Milan Hauth <milahu@gmail.com>
2022-07-05 22:38:33 +0200

    fix

--- src/subprocess-posix.cc
+++ src/subprocess-posix.cc
@@ -287,6 +287,7 @@ Subprocess *SubprocessSet::Add(const string& command, bool use_console) {
     delete subprocess;
     return 0;
   }
+  fprintf(stderr, "SubprocessSet::Add: pid %i: use_console = %s\n", subprocess->pid_, use_console);
   running_.push_back(subprocess);
   return subprocess;
 }
